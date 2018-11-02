e77494e7796ff79975fbc990c9bd328a8509acdd
youn lee <iyeonseog@iyeonseog-ui-macbook-air.local>
2018-11-02 13:58:36 +0900

    Initial Commit

---
+++ MyFirstSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstSwift
+//
+//  Created by 이연석 on 2018. 11. 2..
+//  Copyright © 2018년 이연석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
